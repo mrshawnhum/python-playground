d70819e7496dcee7d813ae8985b86e43fb955525
Shawn Humphreys <shawnhum97@gmail.com>
2020-11-30 13:28:54 -0600

    Practice if statements

--- eat.py
+++ eat.py
@@ -1,2 +1,7 @@
-price = 5
-print(not price == 10)
+price = 20
+
+if price > 22:
+    print("That'll be " + str(price) + " please!")
+    print("Thank you for your order!")
+elif price < 22:
+    print("Please add $" + str(int(22 - price )) + " to complete order!")
