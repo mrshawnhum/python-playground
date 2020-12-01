6d455d462b0eccf1566dd2116db8e55a47f9d42d
Shawn Humphreys <shawnhum97@gmail.com>
2020-11-30 18:10:37 -0600

    Practice print formatting

--- eat.py
+++ eat.py
@@ -1,9 +1,8 @@
-names = [
-    "Shawn",
-    "Manav",
-    "Courtney",
-    "Dyeshia",
-    "Connor"
-]
-names[0] = "Shawn Humphreys"
-print(names[0:3])
+print("This is a string {}".format("INSERTED"))
+
+print("The {1} {0} {2}".format("Old", "Crazy", "Man"))
+
+print("The {s} {h} {f}".format(s="Sleepy", h="Hungry", f="Fisherman"))
+
+name = "Shawn"
+print(f"Hello, my name is {name}")
