6255e3d78b456e50607e6e130c0e8cd34edd58c1
Shawn Humphreys <shawnhum97@gmail.com>
2020-11-30 20:28:30 -0600

    Practice tuples

--- eat.py
+++ eat.py
@@ -1,9 +1,8 @@
-my_dict = {"key1": "value1", "key2": "value2"}
+# tuple
+m = (1, 2, 3)
 
-print(my_dict["key1"])
+# list
+my_list = [1, 2, 3]
 
-prices = {"apple": 2.99, "oranges": 1.99, "milk": 4.99}
-print(prices["milk"])
-
-d = {"key1":['a', 'b', 'c', 'd']}
-print(d["key1"][1].upper())
+print(m[0:2])
+print(my_list[0:2])
