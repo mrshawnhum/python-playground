0025b5a3eb0c63cd4d41e91734e4949e004bf37b
Shawn Humphreys <shawnhum97@gmail.com>
2020-12-01 19:17:01 -0600

    PRactice functions in Python

--- eat.py
+++ eat.py
@@ -1,17 +1,4 @@
-myfile = open("myfile.txt")
-print(myfile.read())
-myfile.seek(0)
-print(myfile.read())
+def names():
+    print("Shawn")
 
-myfile.seek(0)
-myfile.readlines()
-
-with open("myfile.txt", mode="a") as f:
-    f.write("Add new file contexts using write method")
-    print(f)
-
-with open("example.txt", mode="w") as tx:
-    tx.write("File was created using python")
-
-with open("example.txt", mode="r") as tx:
-    print(tx.read())
+names()
