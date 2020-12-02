a364ec5ef08a4b82718aa58c5820fc917f78820e
Shawn Humphreys <shawnhum97@gmail.com>
2020-12-01 20:32:39 -0600

    Practice functions with tuples

--- eat.py
+++ eat.py
@@ -1,4 +1,17 @@
-def names():
-    print("Shawn")
+work_hours = [("Abby", 100), ("Billy", 400), ("Cassie", 800)]
 
-names()
+def employee_check(work_hours):
+    current_max = 0
+    employee_of_month = ''
+
+    for employee,hours in work_hours:
+        if hours > current_max:
+            current_max = hours
+            employee_of_month = employee
+        else:
+            pass
+
+    return(employee_of_month, current_max)
+
+result = employee_check(work_hours)
+print(result)
