c9dea1aa714ba558dcdd1c41adb3ae0846735c90
Shawn Humphreys <shawnhum97@gmail.com>
2021-01-12 10:17:41 -0600

    🎨Show example of decorators

--- eat.py
+++ eat.py
@@ -1,11 +1,37 @@
-class Dog():
-    def __init__(self,breed,name,spots):
-        self.breed = breed
-        self.name = name
-        self.spots = spots
+def func():
+    return 1
 
-my_dog = Dog(breed='Lab', name='Daisy', spots=False)
+example = func()
+print(example)
 
-print(my_dog.breed)
-print(my_dog.spots)
-print(my_dog.name)
+def hello():
+    return "Hi Shawn!"
+
+def other(some_def_func):
+    print("Other code here")
+    print(some_def_func())
+
+other(hello)
+
+def new_dec(orginal_func):
+    def wrap_func():
+
+        print("Some extra code")
+
+        orginal_func()
+
+        print("Some more code")
+    
+    return wrap_func
+
+def fuc_needs_dec():
+    print("Lets get decorated here")
+
+decorated_func = new_dec(fuc_needs_dec)
+decorated_func()
+
+@new_dec
+def fuc_needs_dec():
+    print("This decorator works!")
+
+fuc_needs_dec()
