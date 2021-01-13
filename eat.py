721299dc627eb4d56e48e498c877850d2c713761
Shawn Humphreys <shawnhum97@gmail.com>
2021-01-12 21:43:23 -0600

    Show examples of reading and opening files

--- eat.py
+++ eat.py
@@ -1,18 +1,12 @@
-from collections import Counter
-from collections import defaultdict
-from collections import namedtuple
+import os
+import shutil
+import send2trash
 
-mylist = [1, 1, 1, 2, 2, 3, 2]
+# f = open('eat.txt', 'w+')
+# f.write('This is a test file')
+# f.close()
 
-# print(Counter(mylist))
+# os.getcwd()
 
-d = {'a':10}
-print(d)
-
-d = defaultdict(lambda: 0)
-d['correct'] = 100
-print(d['correct'])
-
-Dog = namedtuple('Dog', ['age', 'breed', 'name'])
-sammy = Dog(age=5, breed='Husky', name='Same')
-print(sammy)
+# shutil.move('c:/Users/mrsha/Code/Python/hangman/eat.txt', os.getcwd())
+# send2trash.send2trash('eat.txt')
